d06981c3a234f3e3e454e75d1924aef72f0a7fb2
Martin Schatz <martin.schatz@gmail.com>
2021-02-22 23:45:20 +0100

    Finished poor-mans-Virtual-IP. Its not pretty, but it works... found in dhcpcd.exit-hook.j2

--- files/generate_ipaddress.sh
+++ files/generate_ipaddress.sh
@@ -13,14 +13,50 @@ function getDev {
 	}
 
 function getIP {
-	if echo "${devList}" | grep -q "${1} UP"
+	if echo ${devList} | grep -q "${1} UP"
 	then
 		ip address show dev ${1} | grep -Po '(?<=inet )((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])/([1-9]|[1-2][0-9]|3[0-2])(?= )'
-	elif echo "${devList}" | grep -q "${1} DOWN"
+	elif echo "${devList}" | grep -q "^${1} DOWN$"
 	then
 		echo "DOWN"
 	else
 		echo "MISSING"
+	fi
+	}
+
+function modIP {
+	operation=${1}
+	ip_address=${2}
+	device=$(echo "${3}" | cut -f 1 -d ":")
+	device_full=${3}
+	if [ "${device}" = "LAN" -o "${device}" = "WAN" ]
+	then
+		devicetype="${device}"
+		eval device=\"\$dev${device}\"
+		eval netIP=\"\$ip${devicetype}\"
+	elif [ "${device}" = "${devLAN}" ]
+	then
+		devicetype="LAN"
+		netIP="${ipLAN}"
+	elif [ "${device}" = "${devWLAN}" ]
+	then
+		devicetype="WLAN"
+		netIP="${ipWLAN}"
+	fi
+
+	if [ "${operation}" = "add" ]
+	then
+		if ! echo "${netIP}" | grep -q "${ip_address}"
+		then
+			echo "ip address add ${ip_address} dev ${device_full}"
+			echo "arp -d ${ip_address}"
+		fi
+	elif [ "${operation}" = "del" ]
+	then
+		if echo "${netIP}" | grep -q "${ip_address}"
+		then
+			echo "ip address del ${ip_address} dev ${device}"
+		fi
 	fi
 	}
 
@@ -30,7 +66,6 @@ function leadingDevice {
 	do
 		eval netDev=\"\$dev${device}\"
 		eval netIP=\"\$ip${device}\"
-
 		if [ "${netIP}" != "DOWN" -o "${netIP}" != "MISSING" ]
 		then
 			returnString="${device}"
@@ -42,23 +77,23 @@ function leadingDevice {
 function leadingDeviceName {
 	if [ "${devActive}" = "LAN" -o "${devActive}" = "WLAN" ]
 	then
-		eval netDev=\"\$dev${device}\"
+		eval netDev=\"\$dev${devActive}\"
 		echo "${netDev}"
 	else
 		echo "NONE"
 	fi
 	}
 
-function delActiveGlobal {
+function delInctiveIP {
 	for device in LAN WLAN;
 	do
 		if [ "${devActive}" != "${device}" ]
 		then
 			eval netDev=\"\$dev${device}\"
 			eval netIP=\"\$ip${device}\"
-			if echo "netIP" | grep -q "${ipFull}"
+			if echo "${netIP}" | grep -q "${ipFull}"
 			then
-				echo "ip address del ${ipFull} dev $(netDev)"
+				modIP del ${ipFull} ${netDev}
 			fi
 		fi
 	done
@@ -67,7 +102,6 @@ function delActiveGlobal {
 function getLastOctett {
 	eval net_prefix=\"\$ip${devActive}_prefix\"
 	eval netIP=\"\$ip${devActive}\"
-	echo "getLastOctett: ${devActive}"
 	echo "${netIP}" | grep "^${net_prefix}" | cut -f 4 -d "." 
 	}
 
@@ -76,18 +110,14 @@ devActive=$(leadingDevice)
 ipLAN="$(getIP ${devLAN})"
 ipWLAN="$(getIP ${devWLAN})"
 
+echo "devActive=${devActive}"
+
 if echo "${ipGlobal_prefix}" | grep -Po '^((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])$'
 then
 	ipFull="${ipGlobal_prefix}"
 else
 	ipFull="${ipGlobal_prefix}$(getLastOctett)"
 fi
-
-echo "devList: ${devList}" >&2
-echo "devActive: ${devActive}" >&2
-echo "ipLAN: ${ipLAN}" >&2
-echo "ipWLAN: ${ipWLAN}" >&2
-echo "ipFull: ${ipFull}" >&2
 
 devLANcount=1
 devWLANcount=1
@@ -96,19 +126,19 @@ if [ "${ipserveNet}" != "" -a "${ipserveNet}" != "NONE" ]
 then
 	if [ "${ipLAN}" != "DOWN" -o "${ipLAN}" != "MISSING" ]
 	then
-		echo "ip address add ${ipserveNet} dev ${devLAN}:${devLANcount}"
+		modIP add ${ipserveNet} ${devLAN}:${devLANcount}
 		devLANcount=$( expr ${devLANcount} + 1 )
 	else
-		echo "ip address del ${ipserveNet} dev ${devLAN}"
+		modIP del ${ipserveNet} ${devLAN}
 	fi
 fi
 
 if [ "${devActive}" = "LAN" ]
 then
-	echo "ip address add ${ipFull} dev $(leadingDeviceName):${devLANcount}"
-	echo "arp -d ${ipFull}"
+	delInctiveIP
+	modIP add ${ipFull} $(leadingDeviceName):${devLANcount}
 elif [ "${devActive}" = "WLAN" ]
 then
-	echo "ip address add ${ipFull} dev $(leadingDeviceName):${devWLANcount}"
-	echo "arp -d ${ipFull}"
+	delInctiveIP
+	modIP add ${ipFull} $(leadingDeviceName):${devWLANcount}
 	fi
